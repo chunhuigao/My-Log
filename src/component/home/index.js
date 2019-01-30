8e4d47a3b2f18eb85173504a992804c06143e49f
ChunhuiGao <1912128546@qq.com>
2019-01-30 21:26:59 +0800

    判断变量是否为数组

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import Commmon from '../../public/common.js'
 class Home extends Component {
     componentDidMount(){
-        Commmon.replaceParamVal('id','yinyin')
+        Commmon.replaceParamVal('id','yinyin');
+        let arr = new Array(1)
+        console.log(Commmon.isArray(arr))
     }
   render() {
     return (
       <div>
-        替换URL指定参数
+        <p>替换URL指定参数</p> 
+        <p>判断对象是否属于数组</p>
       </div>
     )
   }
--- src/public/common.js
+++ src/public/common.js
@@ -1,13 +1,21 @@
 
 
 const Common = {
-    replaceParamVal:(paramName,replaceVal)=>{
+    replaceParamVal:(paramName, replaceVal)=>{
         let oldUrl = window.location.href.toString();  
         let re= eval('/('+ paramName+'=)([^&]*)/gi');
         window.history.replaceState(null,null,oldUrl.replace(re,paramName+'='+replaceVal))
         
         
+    },
+    isArray:(arg) => {
+        if (typeof Array.isArray === "function") {
+            return Array.isArray(arg); 
+        }else{
+            return Object.prototype.toString.call(arg) === '[object Array]';
+        }
     }
+  
 }
 
 
